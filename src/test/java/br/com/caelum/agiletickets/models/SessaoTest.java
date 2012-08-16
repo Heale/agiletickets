7d58ca27538e4b3373f7379dbfa64af4632713d6
waltermsbr <waltermsbr@hotmail.com>
2012-08-16 19:02:20 -0300

    Implementando Builder.

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -5,28 +5,22 @@ import org.junit.Test;
 
 public class SessaoTest {
 
-
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
-
-        Assert.assertTrue(sessao.podeReservar(1));
+		Sessao sessao = new SessaoBuilder().getSessao(2);
+		Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
-
+		Sessao sessao = new SessaoBuilder().getSessao(2);
 		Assert.assertFalse(sessao.podeReservar(3));
 	}
 
 	@Test
-	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(5);
-
+	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis()
+			throws Exception {
+		Sessao sessao = new SessaoBuilder().getSessao(5);
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
